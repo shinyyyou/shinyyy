2fd702975bc556d50bca5ad394c8facfa08eb2f4
shinyyyou <samya.ishii@escola.pr.gov.br>
2025-09-09 20:59:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,10 +63,18 @@ const products = [
   },
   {
     id: 9,
+    name: "Pó Facial Peach Power-Dapop",
+    price: 14.90,
+    image: "img/unnamed(2).jpg",
+    stock: 2
+  
+  },
+  {
+    id: 10,
     name: "Pó Facial Rosa MOsqueta Fenzza",
     price: 14.90,
-    image: "img/unnamed(1).jpg",
-    stock: 6
+    image: "img/unnamed(2).jpg",
+    stock: 2
   
   }
 ];
@@ -326,6 +334,7 @@ document.addEventListener('keydown', function(event) {
         closeCart();
     }
 });
+
 
 
 
