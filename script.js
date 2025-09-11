2e837a52bf66cdb75fd22d650a6e5aadd7a9181c
shinyyyou <samya.ishii@escola.pr.gov.br>
2025-09-10 22:36:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const products = [
     { id: 2, name: "Blush Safira Cores Variadas", price: 19.90, image: "img/179691af-2ffe-4378-b356-1fab83238041.jpeg", stock: 5 },
     { id: 3, name: "Iluminador Safira Cores Variadas", price: 13.90, image: "img/652ff3fb-9658-4305-b56b-0f8addd8934c.jpeg", stock: 5 },
     { id: 4, name: "Máscara de Cílios Efeito Boneca Safira", price: 14.90, image: "img/a5d9b425-dad8-4be2-a277-d14e0511435e.jpeg", stock: 0 },
-    { id: 5, name: "Base líquida SARAH`S BEAUTY", price: 14.90, image: "img/unnamed.jpg", stock: 6 },
+    { id: 5, name: "Base líquida SARAH`S BEAUTY", price: 14.90, image: "img/unnamed (5).jpg", stock: 6 },
     { id: 6, name: "Paleta de iluminador LOVE-max Love", price: 19.90, image: "img/775392d6-299c-4535-8f79-4dc112f15cfe.jpeg", stock: 3 },
     { id: 7, name: "Corretivo Líquido Lua & Neve", price: 14.90, image: "img/cb9c3f40-ece2-41ab-923c-90b0bd6105e2.jpeg", stock: 6 },
     { id: 8, name: "Pó Facial Rosa MOsqueta Fenzza", price: 14.90, image: "img/unnamed (1).jpg", stock: 2 },
@@ -171,3 +171,4 @@ document.getElementById("finish-btn").addEventListener("click", () => {
 });
 
 // =====
+
